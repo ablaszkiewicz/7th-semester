6e45a29b94c0e0f2e0bc8c3c39068689d56d52af
Aleksander Błaszkiewicz <54682207+ablaszkiewicz@users.noreply.github.com>
2022-10-20 18:35:52 +0200

    Add procesy initial files

---
+++ programowanie-aplikacji-lokalnych/procesy/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace procesy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private GridViewColumnHeader listViewSortCol = null;
+        private SortAdorner listViewSortAdorner = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<User> items = new List<User>();
+            items.Add(new User() { Name = "John Doe", Age = 42, Sex = SexType.Male });
+            items.Add(new User() { Name = "Jane Doe", Age = 39, Sex = SexType.Female });
+            items.Add(new User() { Name = "Sammy Doe", Age = 13, Sex = SexType.Male });
+            items.Add(new User() { Name = "Donna Doe", Age = 13, Sex = SexType.Female });
+            lvUsers.ItemsSource = items;
+
+            var view = (CollectionView)CollectionViewSource.GetDefaultView(lvUsers.ItemsSource);
+            view.Filter = UserFilter;
+        }
+
+        private bool UserFilter(object item)
+        {
+            if (String.IsNullOrEmpty(txtFilter.Text))
+                return true;
+            else
+                return ((item as User).Name.IndexOf(txtFilter.Text, StringComparison.OrdinalIgnoreCase) >= 0);
+        }
+
+        private void txtFilter_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            CollectionViewSource.GetDefaultView(lvUsers.ItemsSource).Refresh();
+        }
+
+        private void lvUsersColumnHeader_Click(object sender, RoutedEventArgs e)
+        {
+            GridViewColumnHeader column = (sender as GridViewColumnHeader);
+            string sortBy = column.Tag.ToString();
+            if (listViewSortCol != null)
+            {
+                AdornerLayer.GetAdornerLayer(listViewSortCol).Remove(listViewSortAdorner);
+                lvUsers.Items.SortDescriptions.Clear();
+            }
+
+            ListSortDirection newDir = ListSortDirection.Ascending;
+            if (listViewSortCol == column && listViewSortAdorner.Direction == newDir)
+                newDir = ListSortDirection.Descending;
+
+            listViewSortCol = column;
+            listViewSortAdorner = new SortAdorner(listViewSortCol, newDir);
+            AdornerLayer.GetAdornerLayer(listViewSortCol).Add(listViewSortAdorner);
+            lvUsers.Items.SortDescriptions.Add(new SortDescription(sortBy, newDir));
+        }
+    }
+
+    public enum SexType { Male, Female };
+
+    public class User
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Mail { get; set; }
+
+        public SexType Sex { get; set; }
+    }
+}
