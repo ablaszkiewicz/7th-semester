98dc7a869c23394ee9d7f63f230688991acbf662
Aleksander Błaszkiewicz <54682207+ablaszkiewicz@users.noreply.github.com>
2022-11-12 13:54:20 +0100

    Move mvc files

---
+++ realizacja-aplikacji-internetowych-RAI/mvc/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using mvc.Models;
+
+namespace mvc.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult RedirectToUser()
+    {
+        if (Request.Form["username"] == "admin")
+        {
+            return RedirectToAction("Index", "Admin", new { username = Request.Form["username"] });
+        }
+        else
+        {
+            return RedirectToAction("Index", "User", new { username = Request.Form["username"] });
+        }
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
